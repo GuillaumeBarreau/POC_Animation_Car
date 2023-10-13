f796fa8c3a1fbe0c82f931c7b82358a317483018
Guillaume Barreau <guillaume.barreau@raisepartner.com>
2023-10-12 23:50:31 -0400

    feat: :sparkles: Init POC Animation Car

---
+++ script.js
@@ -0,0 +1,557 @@
+const imagesCar = [
+    './assets/images/vroumvroum/000018.png',
+    './assets/images/vroumvroum/000019.png',
+    './assets/images/vroumvroum/000020.png',
+    './assets/images/vroumvroum/000021.png',
+    './assets/images/vroumvroum/000022.png',
+    './assets/images/vroumvroum/000023.png',
+    './assets/images/vroumvroum/000024.png',
+    './assets/images/vroumvroum/000025.png',
+    './assets/images/vroumvroum/000026.png',
+    './assets/images/vroumvroum/000027.png',
+    './assets/images/vroumvroum/000028.png',
+    './assets/images/vroumvroum/000029.png',
+    './assets/images/vroumvroum/000030.png',
+    './assets/images/vroumvroum/000031.png',
+    './assets/images/vroumvroum/000032.png',
+    './assets/images/vroumvroum/000033.png',
+    './assets/images/vroumvroum/000034.png',
+    './assets/images/vroumvroum/000035.png',
+    './assets/images/vroumvroum/000036.png',
+    './assets/images/vroumvroum/000037.png',
+    './assets/images/vroumvroum/000038.png',
+    './assets/images/vroumvroum/000039.png',
+    './assets/images/vroumvroum/000040.png',
+    './assets/images/vroumvroum/000041.png',
+    './assets/images/vroumvroum/000042.png',
+    './assets/images/vroumvroum/000043.png',
+    './assets/images/vroumvroum/000044.png',
+    './assets/images/vroumvroum/000045.png',
+    './assets/images/vroumvroum/000046.png',
+    './assets/images/vroumvroum/000047.png',
+    './assets/images/vroumvroum/000048.png',
+    './assets/images/vroumvroum/000049.png',
+    './assets/images/vroumvroum/000050.png',
+    './assets/images/vroumvroum/000051.png',
+    './assets/images/vroumvroum/000052.png',
+    './assets/images/vroumvroum/000053.png',
+    './assets/images/vroumvroum/000054.png',
+    './assets/images/vroumvroum/000055.png',
+    './assets/images/vroumvroum/000056.png',
+    './assets/images/vroumvroum/000057.png',
+    './assets/images/vroumvroum/000058.png',
+    './assets/images/vroumvroum/000059.png',
+    './assets/images/vroumvroum/000060.png',
+    './assets/images/vroumvroum/000061.png',
+    './assets/images/vroumvroum/000062.png',
+    './assets/images/vroumvroum/000063.png',
+    './assets/images/vroumvroum/000064.png',
+    './assets/images/vroumvroum/000065.png',
+    './assets/images/vroumvroum/000066.png',
+    './assets/images/vroumvroum/000067.png',
+    './assets/images/vroumvroum/000068.png',
+    './assets/images/vroumvroum/000069.png',
+    './assets/images/vroumvroum/000070.png',
+    './assets/images/vroumvroum/000071.png',
+    './assets/images/vroumvroum/000072.png',
+    './assets/images/vroumvroum/000073.png',
+    './assets/images/vroumvroum/000074.png',
+    './assets/images/vroumvroum/000075.png',
+    './assets/images/vroumvroum/000076.png',
+    './assets/images/vroumvroum/000077.png',
+    './assets/images/vroumvroum/000078.png',
+    './assets/images/vroumvroum/000079.png',
+    './assets/images/vroumvroum/000080.png',
+    './assets/images/vroumvroum/000081.png',
+    './assets/images/vroumvroum/000082.png',
+    './assets/images/vroumvroum/000083.png',
+    './assets/images/vroumvroum/000084.png',
+    './assets/images/vroumvroum/000085.png',
+    './assets/images/vroumvroum/000086.png',
+    './assets/images/vroumvroum/000087.png',
+    './assets/images/vroumvroum/000088.png',
+    './assets/images/vroumvroum/000089.png',
+    './assets/images/vroumvroum/000090.png',
+    './assets/images/vroumvroum/000091.png',
+    './assets/images/vroumvroum/000092.png',
+    './assets/images/vroumvroum/000093.png',
+    './assets/images/vroumvroum/000094.png',
+    './assets/images/vroumvroum/000095.png',
+    './assets/images/vroumvroum/000096.png',
+    './assets/images/vroumvroum/000097.png',
+    './assets/images/vroumvroum/000098.png',
+    './assets/images/vroumvroum/000099.png',
+    './assets/images/vroumvroum/000100.png',
+    './assets/images/vroumvroum/000101.png',
+    './assets/images/vroumvroum/000102.png',
+    './assets/images/vroumvroum/000103.png',
+    './assets/images/vroumvroum/000104.png',
+    './assets/images/vroumvroum/000105.png',
+    './assets/images/vroumvroum/000106.png',
+    './assets/images/vroumvroum/000107.png',
+    './assets/images/vroumvroum/000108.png',
+    './assets/images/vroumvroum/000109.png',
+    './assets/images/vroumvroum/000110.png',
+    './assets/images/vroumvroum/000111.png',
+    './assets/images/vroumvroum/000112.png',
+    './assets/images/vroumvroum/000113.png',
+    './assets/images/vroumvroum/000114.png',
+    './assets/images/vroumvroum/000115.png',
+    './assets/images/vroumvroum/000116.png',
+    './assets/images/vroumvroum/000117.png',
+    './assets/images/vroumvroum/000118.png',
+    './assets/images/vroumvroum/000119.png',
+    './assets/images/vroumvroum/000120.png',
+    './assets/images/vroumvroum/000121.png',
+    './assets/images/vroumvroum/000122.png',
+    './assets/images/vroumvroum/000123.png',
+    './assets/images/vroumvroum/000124.png',
+    './assets/images/vroumvroum/000125.png',
+    './assets/images/vroumvroum/000126.png',
+    './assets/images/vroumvroum/000127.png',
+    './assets/images/vroumvroum/000128.png',
+    './assets/images/vroumvroum/000129.png',
+    './assets/images/vroumvroum/000130.png',
+    './assets/images/vroumvroum/000131.png',
+    './assets/images/vroumvroum/000132.png',
+    './assets/images/vroumvroum/000133.png',
+    './assets/images/vroumvroum/000134.png',
+    './assets/images/vroumvroum/000135.png',
+    './assets/images/vroumvroum/000136.png',
+    './assets/images/vroumvroum/000137.png',
+    './assets/images/vroumvroum/000138.png',
+    './assets/images/vroumvroum/000139.png',
+    './assets/images/vroumvroum/000140.png',
+    './assets/images/vroumvroum/000141.png',
+    './assets/images/vroumvroum/000142.png',
+    './assets/images/vroumvroum/000143.png',
+    './assets/images/vroumvroum/000144.png',
+    './assets/images/vroumvroum/000145.png',
+    './assets/images/vroumvroum/000146.png',
+    './assets/images/vroumvroum/000147.png',
+    './assets/images/vroumvroum/000148.png',
+    './assets/images/vroumvroum/000149.png',
+    './assets/images/vroumvroum/000150.png',
+    './assets/images/vroumvroum/000151.png',
+    './assets/images/vroumvroum/000152.png',
+    './assets/images/vroumvroum/000153.png',
+    './assets/images/vroumvroum/000154.png',
+    './assets/images/vroumvroum/000155.png',
+    './assets/images/vroumvroum/000156.png',
+    './assets/images/vroumvroum/000157.png',
+    './assets/images/vroumvroum/000158.png',
+    './assets/images/vroumvroum/000159.png',
+    './assets/images/vroumvroum/000160.png',
+    './assets/images/vroumvroum/000161.png',
+    './assets/images/vroumvroum/000162.png',
+    './assets/images/vroumvroum/000163.png',
+    './assets/images/vroumvroum/000164.png',
+    './assets/images/vroumvroum/000165.png',
+    './assets/images/vroumvroum/000166.png',
+    './assets/images/vroumvroum/000167.png',
+    './assets/images/vroumvroum/000168.png',
+    './assets/images/vroumvroum/000169.png',
+    './assets/images/vroumvroum/000170.png',
+    './assets/images/vroumvroum/000171.png',
+    './assets/images/vroumvroum/000172.png',
+    './assets/images/vroumvroum/000173.png',
+    './assets/images/vroumvroum/000174.png',
+    './assets/images/vroumvroum/000175.png',
+    './assets/images/vroumvroum/000176.png',
+    './assets/images/vroumvroum/000177.png',
+    './assets/images/vroumvroum/000178.png',
+    './assets/images/vroumvroum/000179.png',
+    './assets/images/vroumvroum/000180.png',
+    './assets/images/vroumvroum/000181.png',
+    './assets/images/vroumvroum/000182.png',
+    './assets/images/vroumvroum/000183.png',
+    './assets/images/vroumvroum/000184.png',
+    './assets/images/vroumvroum/000185.png',
+    './assets/images/vroumvroum/000186.png',
+    './assets/images/vroumvroum/000187.png',
+    './assets/images/vroumvroum/000188.png',
+    './assets/images/vroumvroum/000189.png',
+    './assets/images/vroumvroum/000190.png',
+    './assets/images/vroumvroum/000191.png',
+    './assets/images/vroumvroum/000192.png',
+    './assets/images/vroumvroum/000193.png',
+    './assets/images/vroumvroum/000194.png',
+    './assets/images/vroumvroum/000195.png',
+    './assets/images/vroumvroum/000196.png',
+    './assets/images/vroumvroum/000197.png',
+    './assets/images/vroumvroum/000198.png',
+    './assets/images/vroumvroum/000199.png',
+    './assets/images/vroumvroum/000200.png',
+    './assets/images/vroumvroum/000201.png',
+    './assets/images/vroumvroum/000202.png',
+    './assets/images/vroumvroum/000203.png',
+    './assets/images/vroumvroum/000204.png',
+    './assets/images/vroumvroum/000205.png',
+    './assets/images/vroumvroum/000206.png',
+    './assets/images/vroumvroum/000207.png',
+    './assets/images/vroumvroum/000208.png',
+    './assets/images/vroumvroum/000209.png',
+    './assets/images/vroumvroum/000210.png',
+    './assets/images/vroumvroum/000211.png',
+    './assets/images/vroumvroum/000212.png',
+    './assets/images/vroumvroum/000213.png',
+    './assets/images/vroumvroum/000214.png',
+    './assets/images/vroumvroum/000215.png',
+    './assets/images/vroumvroum/000216.png',
+    './assets/images/vroumvroum/000217.png',
+    './assets/images/vroumvroum/000218.png',
+    './assets/images/vroumvroum/000219.png',
+    './assets/images/vroumvroum/000220.png',
+    './assets/images/vroumvroum/000221.png',
+    './assets/images/vroumvroum/000222.png',
+    './assets/images/vroumvroum/000223.png',
+    './assets/images/vroumvroum/000224.png',
+    './assets/images/vroumvroum/000225.png',
+    './assets/images/vroumvroum/000226.png',
+    './assets/images/vroumvroum/000227.png',
+    './assets/images/vroumvroum/000228.png',
+    './assets/images/vroumvroum/000229.png',
+    './assets/images/vroumvroum/000230.png',
+    './assets/images/vroumvroum/000231.png',
+    './assets/images/vroumvroum/000232.png',
+    './assets/images/vroumvroum/000233.png',
+    './assets/images/vroumvroum/000234.png',
+    './assets/images/vroumvroum/000235.png',
+    './assets/images/vroumvroum/000236.png',
+    './assets/images/vroumvroum/000237.png',
+    './assets/images/vroumvroum/000238.png',
+    './assets/images/vroumvroum/000239.png',
+    './assets/images/vroumvroum/000240.png',
+    './assets/images/vroumvroum/000241.png',
+    './assets/images/vroumvroum/000242.png',
+    './assets/images/vroumvroum/000243.png',
+    './assets/images/vroumvroum/000244.png',
+    './assets/images/vroumvroum/000245.png',
+    './assets/images/vroumvroum/000246.png',
+    './assets/images/vroumvroum/000247.png',
+    './assets/images/vroumvroum/000248.png',
+    './assets/images/vroumvroum/000249.png',
+    './assets/images/vroumvroum/000250.png',
+    './assets/images/vroumvroum/000251.png',
+    './assets/images/vroumvroum/000252.png',
+    './assets/images/vroumvroum/000253.png',
+    './assets/images/vroumvroum/000254.png',
+    './assets/images/vroumvroum/000255.png',
+    './assets/images/vroumvroum/000256.png',
+    './assets/images/vroumvroum/000257.png',
+    './assets/images/vroumvroum/000258.png',
+    './assets/images/vroumvroum/000259.png',
+    './assets/images/vroumvroum/000260.png',
+    './assets/images/vroumvroum/000261.png',
+    './assets/images/vroumvroum/000262.png',
+    './assets/images/vroumvroum/000263.png',
+    './assets/images/vroumvroum/000264.png',
+    './assets/images/vroumvroum/000265.png',
+    './assets/images/vroumvroum/000266.png',
+    './assets/images/vroumvroum/000267.png',
+    './assets/images/vroumvroum/000268.png',
+    './assets/images/vroumvroum/000269.png',
+    './assets/images/vroumvroum/000270.png',
+    './assets/images/vroumvroum/000271.png',
+    './assets/images/vroumvroum/000272.png',
+    './assets/images/vroumvroum/000273.png',
+    './assets/images/vroumvroum/000274.png',
+    './assets/images/vroumvroum/000275.png',
+    './assets/images/vroumvroum/000276.png',
+    './assets/images/vroumvroum/000277.png',
+    './assets/images/vroumvroum/000278.png',
+    './assets/images/vroumvroum/000279.png',
+    './assets/images/vroumvroum/000280.png',
+    './assets/images/vroumvroum/000281.png',
+    './assets/images/vroumvroum/000282.png',
+    './assets/images/vroumvroum/000283.png',
+    './assets/images/vroumvroum/000284.png',
+    './assets/images/vroumvroum/000285.png',
+    './assets/images/vroumvroum/000286.png',
+    './assets/images/vroumvroum/000287.png',
+    './assets/images/vroumvroum/000288.png',
+    './assets/images/vroumvroum/000289.png',
+    './assets/images/vroumvroum/000290.png',
+    './assets/images/vroumvroum/000291.png',
+    './assets/images/vroumvroum/000292.png',
+    './assets/images/vroumvroum/000293.png',
+    './assets/images/vroumvroum/000294.png',
+    './assets/images/vroumvroum/000295.png',
+    './assets/images/vroumvroum/000296.png',
+    './assets/images/vroumvroum/000297.png',
+    './assets/images/vroumvroum/000298.png',
+    './assets/images/vroumvroum/000299.png',
+    './assets/images/vroumvroum/000300.png',
+    './assets/images/vroumvroum/000301.png',
+    './assets/images/vroumvroum/000302.png',
+    './assets/images/vroumvroum/000303.png',
+    './assets/images/vroumvroum/000304.png',
+    './assets/images/vroumvroum/000305.png',
+    './assets/images/vroumvroum/000306.png',
+    './assets/images/vroumvroum/000307.png',
+    './assets/images/vroumvroum/000308.png',
+]
+
+function locomotive() {
+    gsap.registerPlugin(ScrollTrigger)
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector('#main--container'),
+        smooth: true,
+    })
+
+    locoScroll.on('scroll', ScrollTrigger.update)
+
+    ScrollTrigger.scrollerProxy('#main--container', {
+        scrollTop(value) {
+            return arguments.length
+                ? locoScroll.scrollTo(value, 0, 0)
+                : locoScroll.scroll.instance.scroll.y
+        },
+
+        getBoundingClientRect() {
+            return {
+                top: 0,
+                left: 0,
+                width: window.innerWidth,
+                height: window.innerHeight,
+            }
+        },
+
+        pinType: document.querySelector('#main--container').style.transform
+            ? 'transform'
+            : 'fixed',
+    })
+    ScrollTrigger.addEventListener('refresh', () => locoScroll.update())
+    ScrollTrigger.refresh()
+}
+
+locomotive()
+
+const canvas = document.querySelector('canvas')
+const context = canvas.getContext('2d')
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+window.addEventListener('resize', function () {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    render()
+})
+
+const frameCount = 290
+
+const images = []
+const imageSeq = {
+    frame: 1,
+}
+
+for (let i = 0; i < frameCount; i++) {
+    const img = new Image()
+    img.src = imagesCar[i]
+    images.push(img)
+}
+
+gsap.to(imageSeq, {
+    frame: frameCount - 1,
+    snap: 'frame',
+    ease: `none`,
+    scrollTrigger: {
+        scrub: 0.15,
+        trigger: `#content>canvas`,
+        start: `top top`,
+        end: `775% top`,
+        scroller: `#main--container`,
+    },
+    onUpdate: render,
+})
+
+images[1].onload = render
+
+function render() {
+    scaleImage(images[imageSeq.frame], context)
+}
+
+function scaleImage(img, ctx) {
+    var canvas = ctx.canvas
+    var hRatio = canvas.width / img.width
+    var vRatio = canvas.height / img.height
+    var ratio = Math.max(hRatio, vRatio)
+    var centerShift_x = (canvas.width - img.width * ratio) / 2
+    var centerShift_y = (canvas.height - img.height * ratio) / 2
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.drawImage(
+        img,
+        0,
+        0,
+        img.width,
+        img.height,
+        centerShift_x,
+        centerShift_y,
+        img.width * ratio,
+        img.height * ratio,
+    )
+}
+
+function triggerVideo() {
+    const video = document.querySelector('video')
+    video.style.opacity = 1
+    video.play()
+
+    setTimeout(() => {
+        video.style.opacity = 0
+    }, 20000)
+}
+
+function hiddenVideo() {
+    console.log('hiddenVideo')
+    const video = document.querySelector('video')
+    video.style.opacity = 0
+    video.currentTime = 0
+    video.pause()
+}
+
+ScrollTrigger.create({
+    trigger: '#content>canvas',
+    pin: true,
+    scroller: `#main--container`,
+    start: `top top`,
+    end: `800% top`,
+    onLeave: () => triggerVideo(),
+    onEnterBack: () => hiddenVideo(),
+})
+
+gsap.to('#page1', {
+    scrollTrigger: {
+        trigger: `#page1`,
+        start: `top top`,
+        end: `bottom top`,
+        pin: true,
+        scroller: `#main--container`,
+    },
+})
+
+gsap.to('#page2', {
+    scrollTrigger: {
+        trigger: `#page2`,
+        start: `top top`,
+        end: `bottom top`,
+        pin: true,
+        scroller: `#main--container`,
+    },
+})
+
+gsap.to('#section-3', {
+    scrollTrigger: {
+        trigger: `#section-3`,
+        start: `top top`,
+        end: `bottom top`,
+        pin: true,
+        scroller: `#main--container`,
+    },
+})
+
+gsap.to('#section-4', {
+    scrollTrigger: {
+        trigger: '#section-4',
+        start: 'top top',
+        end: 'bottom top',
+        pin: true,
+        scroller: `#main--container`,
+    },
+})
+
+gsap.to('#panel--left', {
+    xPercent: 100,
+    ease: 'nEase',
+    scrollTrigger: {
+        trigger: '#panel--left',
+        start: 'center center',
+        end: 'bottom top',
+        scrub: 1,
+        scroller: `#main--container`,
+    },
+})
+
+gsap.to('#panel--right', {
+    xPercent: -100,
+    ease: 'nEase',
+    scrollTrigger: {
+        trigger: '#panel--left',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: 1,
+        scroller: `#main--container`,
+    },
+})
+
+gsap.to('#page1>#right-text', {
+    xPercent: 200,
+    scrollTrigger: {
+        trigger: '#right-text',
+        start: 'top top',
+        scrub: 1,
+        scroller: `#main--container`,
+    },
+})
+
+gsap.to('#page1>#left-text', {
+    opacity: 1,
+    ease: 'none',
+    scrollTrigger: {
+        trigger: '#left-text',
+        start: 'bottom center',
+        end: 'bottom center',
+        scrub: 1,
+        scroller: `#main--container`,
+    },
+})
+
+gsap.to('#page2>#right-text', {
+    xPercent: -200,
+    scrollTrigger: {
+        trigger: '#page2>#right-text',
+        start: 'top top',
+        scrub: 1,
+        scroller: `#main--container`,
+    },
+})
+
+gsap.to('#page2>#left-text', {
+    opacity: 1,
+    ease: 'none',
+    scrollTrigger: {
+        trigger: '#page2>#left-text',
+        start: 'bottom center',
+        end: 'bottom center',
+        scrub: 1,
+        scroller: `#main--container`,
+    },
+})
+
+gsap.to('#bottom-banner', {
+    xPercent: 100,
+    opacity: 1,
+    ease: 'power1.out',
+    scrollTrigger: {
+        trigger: '#bottom-banner',
+        start: 'bottom center',
+        end: 'bottom center',
+        scrub: 1,
+        scroller: `#main--container`,
+    },
+})
+
+gsap.to('#top-banner', {
+    xPercent: -100,
+    opacity: 1,
+    ease: 'power1.out',
+    scrollTrigger: {
+        trigger: '#top-banner',
+        start: 'bottom center',
+        end: 'bottom center',
+        scrub: 1,
+        scroller: `#main--container`,
+    },
+})
